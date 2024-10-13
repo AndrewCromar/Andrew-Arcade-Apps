011db06d62da57ed4e1260ab8b19ec054b822023
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-10-12 23:21:34 -0700

    feat: remove old stuff

--- Driver/Assets/_Andrew Arcade Driver/Scripts/App.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "App Data", menuName = "App")]
-public class App : ScriptableObject
-{
-    public string appName;
-    public string appDeveloper;
-    public Sprite appIcon;
-}
