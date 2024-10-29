75f1e31ed7e00d57625553f53e71093e3549ac49
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-10-28 22:39:11 -0700

    feat: add text

--- Driver/Assets/_Andrew Arcade Apps/Scripts/Controlers/AppController.cs
+++ Driver/Assets/_Andrew Arcade Apps/Scripts/Controlers/AppController.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AppController : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Text titleText;
+    [SerializeField] private Text developerText;
 
     [Header("Debug")]
     [SerializeField] private AppProfile appProfile;
@@ -13,10 +16,9 @@ public class AppController : MonoBehaviour
         appProfile = _appProfile;
 
         spriteRenderer.sprite = Resources.Load<Sprite>("Apps/" + appProfile.icon);
+        if (spriteRenderer.sprite == null) Debug.LogWarning("Failed to load sprite: " + "Apps/" + appProfile.icon);
 
-        if (spriteRenderer.sprite == null)
-        {
-            Debug.LogWarning("Failed to load sprite: " + "Apps/" + appProfile.icon);
-        }
+        titleText.text = appProfile.title;
+        developerText.text = appProfile.developer;
     }
 }
--- Driver/Assets/_Andrew Arcade Apps/Scripts/Controlers/DriverController.cs
+++ Driver/Assets/_Andrew Arcade Apps/Scripts/Controlers/DriverController.cs
@@ -17,6 +17,7 @@ public class DriverController : MonoBehaviour
     [SerializeField] private float rotationMultiplier = 15;
     [Space]
     [SerializeField] private float scaleSmoothing = 10;
+    [SerializeField] private float textScaleSmoothing = 20;
     [SerializeField] private float selectedScale = 1.5f;
 
     [Header("Debug")]
@@ -75,8 +76,11 @@ public class DriverController : MonoBehaviour
 
             app.rotation = Quaternion.Lerp(app.rotation, Quaternion.Euler(0, Mathf.Clamp(app.position.x * rotationMultiplier, -90, 90), 0), rotationSmoothing * Time.deltaTime);
 
-            float scale = selectionOffset == 0 ? selectedScale : 1;
+            float scale = selected ? selectedScale : 1;
             app.localScale = Vector3.Lerp(app.localScale, new Vector3(scale, scale, 0), scaleSmoothing * Time.deltaTime);
+
+            float textScale = selected ? 1 : 0;
+            app.Find("Text").transform.localScale = Vector3.Lerp(app.Find("Text").transform.localScale, new Vector3(textScale, textScale, 1), textScaleSmoothing * Time.deltaTime);
         }
     }
 
